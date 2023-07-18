2cbbda894fd54ca93be4564ba42fe219e8707064
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2023-07-18 17:27:29 +0300

    edit: now it will auto play

--- index.js
+++ index.js
@@ -22,39 +22,37 @@ function getPlayerChoice(){
 function playRound(playerSelection,computerSelection){
 
     if(playerSelection===computerSelection ){
-        return alert("tie");
+         alert("tie");
     }
     else if (playerSelection==="scissors" && computerSelection==="rock") {
-        return alert("computer won");
+         alert("computer won");
     }
     else if (playerSelection==="scissors" && computerSelection==="paper") {
-        return alert("human won");
+         alert("human won");
     }
     else if (playerSelection==="paper" && computerSelection==="rock"){
-        return alert("human won");
+         alert("human won");
     }
     else if (playerSelection==="paper" && computerSelection==="scissors"){
-        return alert("human won");
+         alert("human won");
     }
     else if (playerSelection==="rock" && computerSelection==="paper") {
-        return alert("computer won");
+         alert("computer won");
     }
     else if (playerSelection==="rock" && computerSelection==="scissors"){
-        return alert("human won");
+         alert("human won");
     }
     else{
-        return alert("nothing");
+         alert("nothing");
     }
 }
 
 function game(){
     let times = prompt("how many rounds do you want to play?");
     for(let i = 0;i<times;i++){
-        
         let playerSelection = getPlayerChoice();
         let computerSelection =getComputerChoice();
         playRound(playerSelection,computerSelection);
     }
 }
-
-
+game()
